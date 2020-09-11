a8784363d91a79e64a569378e5af67fc17be3f03
Lene Saile <hola@lenesaile.com>
2020-09-11 11:30:08 +0200

    Create lenesaile.js

---
+++ _data/sites/lenesaile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "gablaxian.com", // optional, falls back to object key
+  description: "Personal web site",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: "site",
+  },
+  urls: [
+    "https://www.lenesaile.com",
+    "https://www.lenesaile.com/proyectos",
+    "https://www.lenesaile.com/servicios",
+    "https://www.lenesaile.com/sobre",
+    "https://www.lenesaile.com/contacto",
+    "https://www.lenesaile.com/gracias",
+    "https://www.lenesaile.com/aviso-legal",
+    "https://www.lenesaile.com/privacidad",
+    "https://www.lenesaile.com/blog",
+    "https://www.lenesaile.com/blog/posts",
+    "https://www.lenesaile.com/blog/2020/pagina-web-velocidad",
+    "https://www.lenesaile.com/blog/2020/nuevo-proyecto-web",
+  ],
+};
